61961eae2533f4a173bfcef057a5c60da8ea9127
Henry Thomas <henrykun95@gmail.com>
2025-07-30 18:08:49 +0100

    fixed url for vite

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ import parse from 'html-react-parser';
 import './App.css'
 
 function App() {
-  const apiUrl = import.meta.env.REACT_APP_API_URL as string | undefined;
+  const apiUrl = import.meta.env.VITE_API_URL as string | undefined;
   const [formData, setFormData] = useState<{ city: string }>({ city: "" })
   const [errors, setErrors] = useState<{ city?: string }>({})
 
